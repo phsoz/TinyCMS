8db670fcd44e52b2d57a1a1c1383855bb97ac99d
PHSon <phson@protonmail.com>
2023-01-29 23:27:28 +0700

    update migrations

--- src/TinyCMS.Web/Program.cs
+++ src/TinyCMS.Web/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TinyCMS.Infrastructure;
+using TinyCMS.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -12,6 +18,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<TinyDatabaseContext>();
+    db.Database.Migrate();
+}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
