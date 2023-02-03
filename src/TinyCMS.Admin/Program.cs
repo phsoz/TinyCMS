fdf6c2d3d7b517a6f6780a2a4f707776c5c77482
PHSon <phson@protonmail.com>
2023-02-03 08:02:33 +0700

    update admin project

--- src/TinyCMS.Admin/Program.cs
+++ src/TinyCMS.Admin/Program.cs
@@ -1,30 +1,27 @@
-using TinyCMS.Admin.Data;
-using TinyCMS.Infrastructure;
-
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddInfrastructureServices(builder.Configuration);
+
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
